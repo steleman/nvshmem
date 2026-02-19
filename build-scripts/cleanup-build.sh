f7fc404c9a8350bd95bcc4400bf4386337027e41
Stefan Teleman <stefan.teleman@gmail.com>
2026-02-18 20:35:53 -0500

    Cleanup in-source build directory.

--- build-scripts/cleanup-build.sh
+++ build-scripts/cleanup-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+here="`pwd`"
+tophere="`dirname ${here}`"
+version="3.5.19-1"
+srcdir="${tophere}/nvshmem-${version}"
+srcbuilddir="${srcdir}/build"
+
 rm -rf CMakeCache.txt CMakeFiles src *.cmake License.txt \
-  _deps lib bin Makefile nvshmem4py
+  _deps lib bin Makefile nvshmem4py ${srcbuilddir}
 
